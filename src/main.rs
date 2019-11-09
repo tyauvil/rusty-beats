a5e386c587e91f0584b7c9014b0e70c6a200ebe9
ty <ty.auvil@gmail.com>
2019-11-09 09:45:00 -0700

    update to actix-web 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,28 @@
-use actix_web::{server, App, HttpRequest};
-use chrono::prelude::*;
+use ::rusty_beats::beats;
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
 use std::env;
 
-fn beats(_req: &HttpRequest) -> String {
-    let utc = Utc::now();
-    let cet = utc.with_timezone(&FixedOffset::east(3600));
-    let seconds = cet.num_seconds_from_midnight() as f32;
-    let beats = (seconds * 0.011_574) as u32;
-    let mut prefix = "";
-
-    if beats < 10 {
-        prefix = "00"
-    }
-    if beats > 10 && beats < 100 {
-        prefix = "0"
-    }
-
-    format!("@{}{}.beats", prefix, beats)
+fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(beats()))
 }
 
-fn main() {
-    // Get the port number to listen on.
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let port = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
         .expect("PORT must be a number");
 
-    // Start a server, configuring the resources to serve.
-    server::new(|| {
+    HttpServer::new(|| {
         App::new()
-            .resource("/", |r| r.f(beats))
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(index)))
     })
-    .bind(("0.0.0.0", port))
-    .expect("Can not bind to port 8000")
-    .run();
+    .bind(("0.0.0.0", port))?
+    .run()
 }
